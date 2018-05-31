4da8b8b52b790e4e213f810224327109234853a5
Shawn Milochik <shawn@milochik.com>
2018-05-31 16:20:44 -0400

    goimports

--- section5/5-3b.go
+++ section5/5-3b.go
@@ -18,38 +18,37 @@ func main() {
 	if err != nil {
 		log.Fatalf("Failed to ping: %s", err)
 	}
-    _, err = db.Exec(`
+	_, err = db.Exec(`
         CREATE TABLE contacts (
             id SERIAL PRIMARY KEY,
             first VARCHAR(100),
             last VARCHAR(100)
         )
     `)
-    if err != nil{
-        log.Fatalf("Failed to create table: %s\n", err)
-    }
+	if err != nil {
+		log.Fatalf("Failed to create table: %s\n", err)
+	}
 
-    type person struct {
-        first string
-        last string
-    }
-    for _, p := range []person{
-        person{first: "Rob", last: "Pike"},
-        person{first: "Ken", last: "Thompson"},
-        person{first: "Robert", last: "Griesemer"},
-    }{
+	type person struct {
+		first string
+		last  string
+	}
+	for _, p := range []person{
+		person{first: "Rob", last: "Pike"},
+		person{first: "Ken", last: "Thompson"},
+		person{first: "Robert", last: "Griesemer"},
+	} {
 
-        _, err = db.Exec(`
+		_, err = db.Exec(`
             INSERT INTO contacts 
                 (first, last)
             VALUES
                 ($1, $2)
         `, p.first, p.last)
-        if err != nil{
-            log.Fatalf("Failed to insert record: %s\n", err)
-        }
-    }
-
+		if err != nil {
+			log.Fatalf("Failed to insert record: %s\n", err)
+		}
+	}
 
 	fmt.Println("done")
 
